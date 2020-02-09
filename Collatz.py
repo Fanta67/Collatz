0d7271bd204840e1d9799e8bffdde136cbcf7d57
Dylan Kan <fanta67@cs.utexas.edu>
2020-02-08 23:21:44 -0600

    code cleanup and formatting with black, passes all hackerrank tests

--- Collatz.py
+++ Collatz.py
@@ -36,6 +36,7 @@ def collatz_read(s: str) -> List[int]:
 
 cache = [0, 1] + [0 for x in range(999998)]
 
+
 def collatz_eval(i: int, j: int) -> int:
     """
     i the beginning of the range, inclusive
@@ -43,6 +44,7 @@ def collatz_eval(i: int, j: int) -> int:
     return the max cycle length of the range [i, j]
     """
     global cache
+
     maxcyclength = 0
     if i > j:
         temp = i
@@ -51,39 +53,50 @@ def collatz_eval(i: int, j: int) -> int:
     assert i <= j
     assert i >= 0
     assert j >= 0
-    #optimization from quiz 4
-    if (i < j // 2):
+
+    # optimization from quiz 4
+    if i < j // 2:
         i = j // 2
+
     for x in range(i, j + 1):
         curr = x
         cyclength = 1
-        #already stored in cache
+
+        # already stored in cache
         if cache[curr] != 0:
             cyclength = cache[curr]
-        #not in cache yet
-        else :
+
+        # not in cache yet
+        else:
             while curr != 1:
+
                 if curr % 2 == 0:
                     curr //= 2
                 else:
-                    #optimization from quiz 3
+                    # optimization from quiz 3
                     curr += (curr << 1) + 1
-                    #do 2 steps at once for odd numbers
+                    # do 2 steps at once for odd numbers
                     curr //= 2
                     cyclength += 1
-                if (curr < len(cache) and cache[curr] != 0):
+
+                if curr < len(cache) and cache[curr] != 0:
                     cyclength += cache[curr]
                     break
+
                 cyclength += 1
-            #lazily fill cache
+
+            # lazily fill cache
             cache[x] = cyclength
         assert cyclength > 0
+
         if cyclength > maxcyclength:
             maxcyclength = cyclength
-    #could be equal to 0 if i and j are 0
+
+    # could be equal to 0 if i and j are 0
     assert maxcyclength >= 0
     assert maxcyclength >= cyclength
     return maxcyclength
+
 
 # -------------
 # collatz_print
@@ -116,5 +129,6 @@ def collatz_solve(r: IO[str], w: IO[str]) -> None:
         v = collatz_eval(i, j)
         collatz_print(w, i, j, v)
 
-if __name__ == '__main__':
-	collatz_solve(stdin, stdout)
+
+if __name__ == "__main__":
+    collatz_solve(stdin, stdout)
